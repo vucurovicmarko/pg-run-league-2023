6d8cdf1dd3f51700d31fc8b08175aa645b286462
Marko Vucurovic <markovucurovic@gmail.com>
2024-02-06 17:18:32 +0100

    Add html webpack plugin, build to docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
