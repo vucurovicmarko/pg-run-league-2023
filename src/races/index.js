b06fede6eaad0006767e2e72a48ef2755c435799
Marko Vucurovic <markovucurovic@gmail.com>
2024-03-25 12:47:05 +0100

    Race 6 added

--- src/races/index.js
+++ src/races/index.js
@@ -3,5 +3,6 @@ import race2 from "./race-2.js";
 import race3 from "./race-1.js";
 import race4 from "./race-4.js";
 import race5 from "./race-5.js";
+import race6 from "./race-6.js";
 
-export default [race1, race2, race3, race4, race5];
+export default [race1, race2, race3, race4, race5, race6];
